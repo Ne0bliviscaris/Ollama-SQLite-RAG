59577181a00837b08628e062f0190c98019a6e36
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-06-19 16:51:28 +0200

    added docstring

--- db_connect.py
+++ db_connect.py
@@ -2,6 +2,11 @@ import sqlite3
 
 
 def sql_query(query, limit=5):
+    """
+    Connect to database and execute SQL query
+    query: str - SQL query
+    limit: int - limit of results, default 5
+    """
     db = "files/sql-murder-mystery.db"  # database file
     connection = sqlite3.connect(db)  # connect to db
     cursor = connection.cursor()  # create cursor instance
