5b586f309b53c8d283be57f54d5073d4389bcde2
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-06-19 21:02:41 +0200

    measuring model efficiency

--- files/test_docker_connection.py
+++ files/test_docker_connection.py
@@ -1,8 +1,21 @@
 # Test the connection with local Ollama container
+import time
+
 from langchain_community.llms.ollama import Ollama
 from settings import MODEL, MODEL_URL
 
-llm = Ollama(model=MODEL, base_url=MODEL_URL)
+llm = Ollama(model=MODEL, base_url=MODEL_URL, temperature=0)
 
-response = llm.invoke("Hello world")
-print(response)
+# Start timer before invoking
+start_time = time.time()
+
+response = llm.invoke("Say hello, answer with max 10 words.")
+
+# End timer after invoking
+end_time = time.time()
+
+# Calculate response time
+response_time = end_time - start_time
+
+print(f"Odpowiedź od modelu {MODEL}: {response}")
+print(f"Czas odpowiedzi modelu {MODEL}: {response_time:.2f} sekund")
