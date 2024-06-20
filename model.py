31ce213c3ab8fb357ebc354eaa93c4d91d84baa9
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-06-20 23:21:15 +0200

    created timer decorator

--- model.py
+++ model.py
@@ -6,29 +6,35 @@ import files.prompt_templates as templates
 from connections import database_connect, get_db_schema, ollama_connect
 
 
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"\nExecution time of {func.__name__}: {(end_time - start_time):.3f} seconds.")
+        return result
+
+    return wrapper
+
+
 def build_langchain():
     """Builds and returns a language chain with database and Ollama connections."""
     db = database_connect()
-    llm = ollama_connect()
-    prompt = templates.query_translation()
+    llm = ollama_connect(temperature=0)
+    prompt = templates.text_to_query()
     chain = create_sql_query_chain(llm, db, prompt)
     return chain
 
 
+@timer
 def process_query_and_get_response(question):
-    start_time = time.time()  # Rozpoczęcie pomiaru czasu
-
     # Connect to the database and model, get table info
     chain = build_langchain()
 
-    # Compressed question v3
+    # Pass the question to the model
     table_info = get_db_schema()
     input_data = {"question": question, "table_info": table_info, "dialect": "sqlite", "top_k": 1}
 
     # Ask the question and get the response from the model
     response = chain.invoke(input_data)
-
-    end_time = time.time()  # Zakończenie pomiaru czasu
-    print(f"Czas wykonania: {end_time - start_time} sekund.")  # Wyświetlenie czasu wykonania
-
     return response
