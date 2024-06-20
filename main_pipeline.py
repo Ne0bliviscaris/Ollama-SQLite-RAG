e9951bdc7b7a13599472aa83c0b5b0df35af2f29
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-06-20 18:05:32 +0200

    small update

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,8 +1,15 @@
 import db_connect
 import model_connect
 
-db_connect.sql_query("SELECT * from person")
+# db_query = "select * from person"
+# db_query = """
+# select * from crime_scene_report where date = '20180115' and city = 'SQL City'
+# """
+# db_connect.sql_query(db_query)
 
 
-question = "How many people are there?"
-model_connect.prompt(question)
+question = """
+Retrieve murder crime scene report from police department's database from jan.15, 2018 in SQL City
+"""
+model_response = model_connect.prompt(question)
+print(model_response)
