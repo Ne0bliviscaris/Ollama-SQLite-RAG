68901182d4e2285a69fbf3dbaa85e9b7f5055520
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-06-20 23:20:08 +0200

    modifiable model temperature

--- connections.py
+++ connections.py
@@ -3,20 +3,19 @@ import sqlite3
 from langchain_community.chat_models import ChatOllama
 from langchain_community.utilities import SQLDatabase
 
-from files.settings import DB_FILE, MODEL
+from files.settings import DB_FILE, MODEL, TEMPARATURE
 
 
-def sql_query(query, limit=10):
+def sql_query(query):
     """
     Connect to database and execute SQL query
     query: str - SQL query
-    limit: int - limit of results, default 10
     """
     db = DB_FILE  # database file
     connection = sqlite3.connect(db)  # connect to db
     cursor = connection.cursor()  # create cursor instance
 
-    cursor.execute(f"{query} LIMIT {limit}")  # execute query
+    cursor.execute(f"{query}")  # execute query
 
     wyniki = cursor.fetchall()  # fetch all results
     for wiersz in wyniki:
@@ -48,12 +47,15 @@ def database_connect():
     return db
 
 
-def ollama_connect():
+def ollama_connect(temperature=TEMPARATURE):
     """
     Establishes a connection to the Ollama model via Docker.
+
+    Input:
+        temperature: float - The temperature to use for the model.
 
     Returns:
         ChatOllama: An instance of the ChatOllama model with specified parameters.
     """
-    llm = ChatOllama(model=MODEL, temperature=0.01)
+    llm = ChatOllama(model=MODEL, temperature=TEMPARATURE)
     return llm
