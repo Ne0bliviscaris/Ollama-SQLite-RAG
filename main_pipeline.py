fc726251820afffb40ffd673d2de135fd8ab7cf6
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-06-20 23:20:41 +0200

    tweaked question

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,7 +1,16 @@
+import connections
 import model
 
+# connections.sql_query(
+#     """
+# SELECT * FROM crime_scene_report WHERE city = 'SQL City' AND date = '20180115';
+# """
+# )
+
+
 question = """
-Retrieve murder crime scene report from police department's database from jan.15, 2018 in SQL City
+Extract police MURDER type crime scene reports from 'SQL City' at 15 january 2018
 """
 model_response = model.process_query_and_get_response(question)
-print(model_response)
+print(f"Question: {question}")
+print(f"Model answer:\n{model_response}\n")
