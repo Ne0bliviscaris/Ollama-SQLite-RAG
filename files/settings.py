afb4a2bd7c44b82ee86e2c74e4efb7bc10925dc5
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-06-20 18:07:03 +0200

    switched to local ollama model

--- files/settings.py
+++ files/settings.py
@@ -1,11 +1,12 @@
 # Easy Model Switch
-# MODEL = "llama3"
-MODEL = "zephyr:latest"
+MODEL = "llama3"
+# MODEL = "zephyr:latest"
 
-if MODEL == "llama3":
-    MODEL_URL = "http://localhost:11434"
-elif MODEL == "zephyr:latest":
-    MODEL_URL = "http://localhost:55555"
+# Switched to local model
+# if MODEL == "llama3":
+#     MODEL_URL = "http://localhost:11434"
+# elif MODEL == "zephyr:latest":
+#     MODEL_URL = "http://localhost:55555"
 
 
 DB_FILE = r"files\sql-murder-mystery.db"
