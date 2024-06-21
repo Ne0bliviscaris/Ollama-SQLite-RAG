e77344219ed8f4aead36ffc0266a23dd52b6ecd0
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-06-21 13:12:02 +0200

    organized rag pipeline steps

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,32 +1,41 @@
 import db
-import model
-from files.prompt_templates import interpret_query, text_to_query
-
-# Manual query test
-# db.sql_query(
-#     """
-# SELECT DISTINCT p.name AS witness_name, c.city AS crime_location
-#     FROM crime_scene_report AS c
-#     JOIN (SELECT * FROM person WHERE address_street_name = 'Northwestern Dr' AND address_number = (SELECT MAX(address_number) FROM person WHERE address_street_name = 'Northwestern Dr')) AS p1
-#     ON c.city = p1.city
-#     JOIN (SELECT * FROM person WHERE name = 'Annabel') AS p2
-#     ON c.city = p2.city;
-# """
-# )
-
-# STEP 1 - Generate SQL query using AI model
-question = """
-Extract police MURDER type crime scene reports from 'SQL City' at 15 january 2018
-"""
-model_response = model.model_response(question, template=text_to_query(), temperature=0)
-print(f"\nQuestion: \n{question}")
-print(f"Model answer:\n{model_response}")
+import modules.model as model
+from modules.prompt_templates import interpret_query, text_to_query
 
 
-# STEP 2 - Pass the AI generated query straight to database
-query_result = db.sql_query(model_response)
-print(f"\n{query_result}")
+def step1_text_to_sql(question, prints=False):
+    model_response = model.model_response(question=question, template=text_to_query(), temperature=0)
+    if prints == True:
+        print(f"\nQuestion: \n{question}")
+        print(f"Model answer:\n{model_response}")
+    return model_response
 
-# STEP 3 - Let AI interpret the qury results
-rag_answer = model.model_response(query_result, template=interpret_query(), temperature=0)
-print(f"\n{rag_answer}\n")
+
+def step2_sql_query(model_response, prints=False):
+    query_result = db.sql_query(model_response)
+    if prints == True:
+        print(f"SQL Query result:\n{query_result}")
+    return query_result
+
+
+def step3_interpret_query(query_result, prints=False):
+    rag_answer = model.model_response(
+        question=query_result, template=interpret_query(), query_result=query_result, temperature=0
+    )
+    if prints == True:
+        print(f"\n\n\nRAG ANSWER:\n{rag_answer}\n")
+    return rag_answer
+
+
+def rag(question, prints=False):
+    model_response = step1_text_to_sql(question, prints)
+    query_results = step2_sql_query(model_response, prints)
+    rag_answer = step3_interpret_query(query_results, prints)
+    return rag_answer
+
+
+if __name__ == "__main__":
+    question = """
+    Extract police MURDER type crime scene reports from 'SQL City' at 15 january 2018
+    """
+    print(rag(question))
