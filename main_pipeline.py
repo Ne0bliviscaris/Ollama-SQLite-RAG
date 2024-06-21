3b7c3c1d41a06cfad7147aa27ec98d526646a04d
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-06-21 20:47:58 +0200

    refactored code, prepared questions

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,54 +1,60 @@
 import db
 import modules.model as model
-from modules.prompt_templates import interpret_query, text_to_query
+from modules.prompt_templates import conclude, text_to_query
 
 
-def step1_text_to_sql(question, prints=False):
-    """Step 1: Translate text instructions into SQL query"""
-    model_response = model.model_response(question=question, template=text_to_query(), temperature=0)
+def launch_model(model_type, question, template, prints=False):
+    """Run the model and return the response."""
+    model_response = model.model_response(model_type, question=question, template=template, temperature=0)
 
-    # Print the question and model response on demand
+    # Print the user input and model response on demand
     if prints == True:
-        print(f"\nQuestion: \n{question}")
-        print(f"Model answer:\n{model_response}")
+        print(f"\nModel type: \n{model_type}")
+        print(f"\nModel answer:\n{model_response}")
     return model_response
 
 
-def step2_sql_query(model_response, prints=False):
-    """Step 2: Run SQL query"""
+def sql_query(model_response, prints=False):
+    """Run SQL query and return the result"""
+
+    if prints == True:
+        print(f"Forwarding SQL Query")
+
     query_result = db.sql_query(model_response)
 
     # Print the SQL query result on demand
     if prints == True:
-        print(f"SQL Query result:\n{query_result}")
+        print(f"\nSQL Query Result:\n{query_result}")
     return query_result
-
-
-def step3_interpret_query(query_result, prints=False):
-    """Step 3: Interpret SQL query results and provide a final answer and next step instruction."""
-    rag_answer = model.model_response(
-        question=query_result, template=interpret_query(), query_result=query_result, temperature=0
-    )
-
-    # Print the RAG answer on demand
-    if prints == True:
-        print(f"\n\n\nRAG ANSWER:\n{rag_answer}\n")
-    return rag_answer
 
 
 def rag(question, prints=False):
     """Full pipeline to answer a database related question using RAG model."""
-    model_response = step1_text_to_sql(question, prints)
-    query_results = step2_sql_query(model_response, prints)
-    rag_answer = step3_interpret_query(query_results, prints)
+
+    # Step 1: Translate text instructions into SQL query
+    model_response = launch_model("SQL Translator", question, text_to_query(), prints)
+
+    # Step 2: Run SQL query and return the result
+    query_results = sql_query(model_response, prints)
+
+    # Step 3: Interpret SQL query results and provide a final answer
+    rag_answer = launch_model("Detective", query_results, conclude(), prints)
 
     # Print the RAG answer unconditionally
-    print(rag_answer)
     return rag_answer
 
 
 if __name__ == "__main__":
+    # question = """
+    # Extract police MURDER type crime scene reports from 'SQL City' at 15 january 2018
+    # """
     question = """
-    Extract police MURDER type crime scene reports from 'SQL City' at 15 january 2018
+    The  witness resides at the last house on "Northwestern Dr"
     """
-    rag(question)
+    # question = """
+    # find who lives in 'Northwestern Dr', 4919
+    # """
+    # question = """
+    # find out more about Christopher Peteuil with license ID 993845
+    # """
+    rag(question, True)
