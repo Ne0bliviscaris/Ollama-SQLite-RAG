f93827016f26841c0e421f12fb51b0a06cf0d0af
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-06-21 20:48:51 +0200

    sql query testing function

---
+++ test_SQL_Query.py
@@ -0,0 +1,10 @@
+from db import database_connect, sql_query
+
+database = database_connect()
+# Manual query test
+results_string = sql_query(
+    """
+                           SELECT * FROM person where license_id = 993845 
+                           limit 10"""
+)
+print(results_string)
