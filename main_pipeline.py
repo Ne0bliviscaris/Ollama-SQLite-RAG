02cc783154184d741eb94c3fd71c10714452cd85
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-06-21 10:22:28 +0200

    introduced step 2 - run AI generated query

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,16 +1,21 @@
 import connections
 import model
 
+# Manual query test
 # connections.sql_query(
 #     """
 # SELECT * FROM crime_scene_report WHERE city = 'SQL City' AND date = '20180115';
 # """
 # )
 
-
+# STEP 1 - Generate SQL query using AI model
 question = """
 Extract police MURDER type crime scene reports from 'SQL City' at 15 january 2018
 """
-model_response = model.process_query_and_get_response(question)
-print(f"Question: {question}")
-print(f"Model answer:\n{model_response}\n")
+model_response = model.model_response(question)
+# print(f"Question: {question}")
+# print(f"Model answer:\n{model_response}\n")
+
+
+# STEP 2 - Pass the AI generated query straight to database
+query_result = connections.sql_query(model_response)
