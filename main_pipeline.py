55a9fd8b3fac422397eefb3d7270971211c9a698
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-06-21 13:17:32 +0200

    additional comments

--- main_pipeline.py
+++ main_pipeline.py
@@ -6,6 +6,8 @@ from modules.prompt_templates import interpret_query, text_to_query
 def step1_text_to_sql(question, prints=False):
     """Step 1: Translate text instructions into SQL query"""
     model_response = model.model_response(question=question, template=text_to_query(), temperature=0)
+
+    # Print the question and model response on demand
     if prints == True:
         print(f"\nQuestion: \n{question}")
         print(f"Model answer:\n{model_response}")
@@ -15,6 +17,8 @@ def step1_text_to_sql(question, prints=False):
 def step2_sql_query(model_response, prints=False):
     """Step 2: Run SQL query"""
     query_result = db.sql_query(model_response)
+
+    # Print the SQL query result on demand
     if prints == True:
         print(f"SQL Query result:\n{query_result}")
     return query_result
@@ -25,6 +29,8 @@ def step3_interpret_query(query_result, prints=False):
     rag_answer = model.model_response(
         question=query_result, template=interpret_query(), query_result=query_result, temperature=0
     )
+
+    # Print the RAG answer on demand
     if prints == True:
         print(f"\n\n\nRAG ANSWER:\n{rag_answer}\n")
     return rag_answer
@@ -35,6 +41,9 @@ def rag(question, prints=False):
     model_response = step1_text_to_sql(question, prints)
     query_results = step2_sql_query(model_response, prints)
     rag_answer = step3_interpret_query(query_results, prints)
+
+    # Print the RAG answer unconditionally
+    print(rag_answer)
     return rag_answer
 
 
@@ -42,4 +51,4 @@ if __name__ == "__main__":
     question = """
     Extract police MURDER type crime scene reports from 'SQL City' at 15 january 2018
     """
-    print(rag(question))
+    rag(question)
