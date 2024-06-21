a017d0fc7385f01058a2b7733404f14cac2689ff
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-06-21 11:43:22 +0200

    query results to string conversion

--- db.py
+++ db.py
@@ -9,6 +9,9 @@ def sql_query(query):
     """
     Connect to database and execute SQL query
     query: str - SQL query
+
+    Returns:
+        results_string: str - Results of the query as a string
     """
     db = DB_FILE  # database file
     connection = sqlite3.connect(db)  # connect to db
@@ -16,11 +19,20 @@ def sql_query(query):
 
     cursor.execute(f"{query}")  # execute query
 
-    wyniki = cursor.fetchall()  # fetch all results
-    for wiersz in wyniki:
-        print(wiersz)  # print results
+    results = cursor.fetchall()  # fetch all results
+    results_string = query_result_to_string(results)
 
     connection.close()  # close connection
+    return results_string
+
+
+def query_result_to_string(results):
+    """
+    Convert query results to a string, each row from a new line.
+    results: list of tuples - The result from a SQL query execution.
+    """
+    results_string = "\n".join([str(row) for row in results])
+    return results_string
 
 
 def get_db_schema():
