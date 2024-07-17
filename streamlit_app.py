e569768edb2609cbe8a3ce9a29ab02364c8d5b0f
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-07-17 22:09:52 +0200

    refactor

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,8 @@
 import streamlit as st
 
-import modules.ai.model as model
 import modules.db as db
 from modules.ai.prompt_templates import conclude, text_to_query
-
-
-def launch_model(model_type, question, template, prints=False):
-    """Run the model and return the response."""
-    model_response = model.model_response(model_type, question=question, template=template, temperature=0)
-    if prints:
-        st.write(f"\nModel type: \n{model_type}")
-        st.write(f"\nModel answer:\n{model_response}")
-    return model_response
-
-
-def sql_query(model_response, prints=False):
-    """Run SQL query and return the result"""
-    if prints:
-        st.write("Forwarding SQL Query")
-    query_result = db.sql_query(model_response)
-    if prints:
-        st.write(f"\nSQL Query Result:\n{query_result}")
-    return query_result
+from rag_pipeline import launch_model
 
 
 def streamlit_rag(question, prints=False):
