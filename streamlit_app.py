5d3cd0bcae6a1b766321ebb70887a2b77b6669b9
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-07-17 21:49:43 +0200

    structure improvements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 import modules.ai.model as model
-import modules.db as db
+import modules.database.db as db
 from modules.ai.prompt_templates import conclude, text_to_query
 
 
