8695033d5dd51fab6c71ef2a1cc46a5def64a2d8
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-02-13 15:58:34 +0100

    extracting model answer and reasoning separately, converting sql query results to string

--- modules/streamlit.py
+++ modules/streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from modules.ai.model import model_response
+from modules.ai.model import model_response, split_model_answer
 from modules.database.db import execute_sql_query
 from modules.database.query_tools import extract_sql_query
 
@@ -17,8 +17,10 @@ def rag():
         st.header("Translator model Response:")
         with st.spinner("Processing the question"):
             generated_answer = model_response(model_type="SQL Translator", question=question)
-            with st.expander(label="Show full response"):
-                st.write(generated_answer)
+            generated_answer, thinking_process = split_model_answer(generated_answer)
+            st.write(generated_answer)
+            with st.expander(label="Thinking process"):
+                st.write(thinking_process)
 
         st.divider()
 
@@ -27,11 +29,12 @@ def rag():
         st.write(f"SQL Query extracted from model's answer:\n{extracted_query}")
         query_results_df = execute_sql_query(extracted_query)
         with st.expander(label="Query Results:"):
-            st.write(query_results_df)
+            st.dataframe(query_results_df)
 
         st.divider()
         st.header("Detective's conclusion:")
         with st.spinner("Analyzing the results"):
-            detective_answer = model_response(model_type="Detective", question=query_results_df)
+            results_string = query_results_df.to_string(index=False)
+            detective_answer = model_response(model_type="Detective", question=results_string)
             with st.expander(label="Detective model Response:"):
                 st.write(detective_answer)
