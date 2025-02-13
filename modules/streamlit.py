ef6a085ef2ac91387ec82094d04de8b9e5a78cb3
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-02-13 16:36:14 +0100

    separated streamlit from RAG logic

--- modules/streamlit.py
+++ modules/streamlit.py
@@ -13,28 +13,50 @@ def rag():
     """
     question = st.text_area("Enter your question here:", height=150)
     if st.button("Get Answer"):
+        with st.container(key="Translator"):
+            st.header("Translator model Response:")
+            generated_answer, thinking_process = rag_translate(question)
 
-        st.header("Translator model Response:")
-        with st.spinner("Processing the question"):
-            generated_answer = model_response(model_type="SQL Translator", question=question)
-            generated_answer, thinking_process = split_model_answer(generated_answer)
             st.write(generated_answer)
             with st.expander(label="Thinking process"):
                 st.write(thinking_process)
 
-        st.divider()
+        with st.container(key="Query Results"):
+            st.divider()
+            st.header("Translated query:")
+            extracted_query, query_results = process_sql_query(generated_answer)
+            st.write(extracted_query)
+            query_results = execute_sql_query(extracted_query)
+            with st.expander(label="Query Results:"):
+                st.write(query_results)
 
-        st.header("Translated query results:")
-        extracted_query = extract_sql_query(generated_answer)
-        st.write(f"SQL Query extracted from model's answer:\n{extracted_query}")
-        query_results_df = execute_sql_query(extracted_query)
-        with st.expander(label="Query Results:"):
-            st.dataframe(query_results_df)
+        with st.container(key="Detective"):
+            st.divider()
+            st.header("Detective's conclusion:")
 
-        st.divider()
-        st.header("Detective's conclusion:")
-        with st.spinner("Analyzing the results"):
-            results_string = query_results_df.to_string(index=False)
-            detective_answer = model_response(model_type="Detective", question=results_string)
-            with st.expander(label="Detective model Response:"):
+            with st.spinner("Analyzing the results"):
+                detective_answer, thinking_process = rag_detective(query_results)
                 st.write(detective_answer)
+                with st.expander(label="Thinking process"):
+                    st.write(thinking_process)
+
+
+def rag_translate(question):
+    """Translate the natural question to SQL query"""
+    generated_answer = model_response(model_type="SQL Translator", question=question)
+    generated_answer, thinking_process = split_model_answer(generated_answer)
+    return generated_answer, thinking_process
+
+
+def rag_detective(query_results_list):
+    """Detective's conclusion"""
+    detective_answer = model_response(model_type="Detective", question=query_results_list)
+    answer, thinking_process = split_model_answer(detective_answer)
+    return answer, thinking_process
+
+
+def process_sql_query(model_answer):
+    """Handle the SQL query received from the model"""
+    extracted_query = extract_sql_query(model_answer)
+    query_results = execute_sql_query(extracted_query)
+    return extracted_query, query_results
