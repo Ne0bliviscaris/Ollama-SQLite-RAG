4ee74e7865128c0fedcd93289749237b386075b7
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-02-18 12:23:02 +0100

    editable model temperature no longer needed

--- modules/settings.py
+++ modules/settings.py
@@ -2,7 +2,6 @@
 # MODEL = "llama3"
 # MODEL = "zephyr:7b"
 MODEL = "deepseek-r1:1.5b"
-TEMPARATURE = 0  # model creativity
 MODEL_TIMEOUT = 10  # model response timeout in seconds
 
 # Switched to local model
