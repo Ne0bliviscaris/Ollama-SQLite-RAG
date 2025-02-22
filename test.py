ea22c18bc38364a1b98846e443d40eab4b00c90b
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-02-22 14:27:21 +0100

    testing structure and rule following

--- test.py
+++ test.py
@@ -1,8 +1,27 @@
-from modules.new_model import Detective, Translator
+from modules.new_model import Translator
 
-response = Translator("How many murders are there in police database?")
-print("\n\n\n\n\n\n")
+# from modules.PydanticAI import Translator
+
+# response = Translator("find all murder cases from SQL City")
+# response = Translator("Find who lives in the last house in Franklin Ave")
+response = Translator("Find who lives on the Franklin Ave")
+# response = Translator("find all car divers")
+try:
+    print("user_input:\n" + response.full_response["user_input"])
+    print("\nquery:\n" + response.full_response["sql_query"])
+    print("\nthinking:\n" + response.full_response["thinking"])
+    print("\nrules_followed:\n" + response.full_response["rules_followed"])
+except:
+    print("\n\n" + response.full_response)
+
+
+# print(json.dumps(response.full_response, indent=2))
+# print("\n\n\n")
+# print(response.answer)
+# print("\n\n\n")
 
 # conclusion = Detective(response.response)
 
-print(response.answer)
+# print(response.sql_query)
+# print("\n\n\n")
+# print(response.template)
